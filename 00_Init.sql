5bf5eb8f6bd74ad94ba476d1800f4da625a50bed
Enrico Cairo <EnricoCairo@users.noreply.github.com>
2015-12-07 11:41:40 +0100

    Update 00_Init.sql

--- 00_Init.sql
+++ 00_Init.sql
@@ -6,10 +6,6 @@
 --
 -- -----------------------------------------------------------------------------
 
-DROP DATABASE IF EXISTS `sysaux`;
-CREATE DATABASE `sysaux` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `sysaux`;
-
 -- Do NOT forget to fix those hoosk in your my.cnf (or my.ini) file
 
 SET GLOBAL event_scheduler = ON;
@@ -17,3 +13,20 @@ SET GLOBAL general_log     = ON;
 SET GLOBAL slow_query_log  = ON;
 SET GLOBAL log_output      = 'table';
 SET GLOBAL init_connect    = 'CALL `sysaux`.logon_trigger()';
+
+-- Databases creation
+
+DROP DATABASE IF EXISTS `mysysutl`;
+CREATE DATABASE `mysysutl` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+DROP DATABASE IF EXISTS `sysaux`;
+CREATE DATABASE `sysaux` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+-- User sysman will be used in future for the front-end
+-- Initial "grant usage" is necessary to avoid "Error Code: 1396. Operation DROP USER failed for 'sysman'@'localhost'"
+
+GRANT USAGE   ON `sysaux`.* TO 'sysman'@'localhost' IDENTIFIED by 'being_deleted';
+DROP   USER 'sysman'@'localhost';
+CREATE USER 'sysman'@'localhost' IDENTIFIED by 'sysman';
+GRANT EXECUTE ON `mysysutl`.* TO 'sysman'@'localhost';
+GRANT EXECUTE ON `sysaux`.*   TO 'sysman'@'localhost';
