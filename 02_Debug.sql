87adc744818d3b4158b2fc36c9e25f2463315bca
Enrico Cairo <EnricoCairo@users.noreply.github.com>
2015-12-07 11:48:11 +0100

    Update 02_Debug.sql

--- 02_Debug.sql
+++ 02_Debug.sql
@@ -13,17 +13,21 @@
 
 DELIMITER $$
 
-DROP TABLE IF EXISTS `sysaux`.`log`$$
+USE `sysaux`$$
 
-CREATE TABLE `sysaux`.`log` (
+SELECT DATABASE(), VERSION(), NOW(), USER()$$
+
+DROP TABLE IF EXISTS `mysysutl`.`log`$$
+
+CREATE TABLE `mysysutl`.`log` (
 	`log_time`	TIMESTAMP(6)	DEFAULT CURRENT_TIMESTAMP(6),
 	`conn_id`	INT(11)			NOT NULL DEFAULT '0',
 	`msg`		VARCHAR(512)	DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='Debug log'$$
 
-DROP PROCEDURE IF EXISTS `sysaux`.`log_setup`$$
+DROP PROCEDURE IF EXISTS `mysysutl`.`log_setup`$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sysaux`.`log_setup` ()
+CREATE DEFINER='root'@'localhost' PROCEDURE `mysysutl`.`log_setup` ()
 DETERMINISTIC CONTAINS SQL
 BEGIN
 	DECLARE log_chk INT DEFAULT 0;
@@ -47,15 +51,15 @@ BEGIN
 	
 END$$
 
-DROP PROCEDURE IF EXISTS `sysaux`.`logger`$$
+DROP PROCEDURE IF EXISTS `mysysutl`.`logger`$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sysaux`.`logger` (
+CREATE DEFINER='root'@'localhost' PROCEDURE `mysysutl`.`logger` (
 	IN logMsg VARCHAR(512)
 ) DETERMINISTIC MODIFIES SQL DATA
 BEGIN
 	DECLARE CONTINUE HANDLER FOR 1146 -- Table not found
 	BEGIN
-		CALL `sysaux`.`log_setup`();
+		CALL `mysysutl`.`log_setup`();
 
 		INSERT INTO `tmp_log` (conn_id, msg) VALUES (CONNECTION_ID(), logMsg);
 	END;
@@ -65,23 +69,23 @@ END$$
 
 DROP PROCEDURE IF EXISTS `sysaux`.`log_cleanup`$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sysaux`.`log_cleanup` (
+CREATE DEFINER='root'@'localhost' PROCEDURE `mysysutl`.`log_cleanup` (
 	IN logMsg VARCHAR(512)
 ) DETERMINISTIC MODIFIES SQL DATA
 BEGIN
-	CALL `sysaux`.`logger`(CONCAT("cleanup() ", IFNULL(logMsg, '')));
+	CALL `mysysutl`.`logger`(CONCAT("cleanup() ", IFNULL(logMsg, '')));
     
-	INSERT INTO `sysaux`.`log` SELECT * FROM `tmp_log`;
+	INSERT INTO `mysysutl`.`log` SELECT * FROM `tmp_log`;
 	DROP TABLE `tmp_log`;
 END$$
 
-DROP EVENT IF EXISTS `sysaux`.`log_purge`$$
+DROP EVENT IF EXISTS `mysysutl`.`log_purge`$$
 
-CREATE DEFINER=`root`@`localhost` EVENT `log_purge`
+CREATE DEFINER='root'@'localhost' EVENT `log_purge`
 ON SCHEDULE EVERY 1 DAY STARTS TIMESTAMP(DATE_ADD(CURRENT_DATE, INTERVAL 1 DAY), '00:05:00') + INTERVAL 1 DAY
 COMMENT 'log cleanup'
 DO BEGIN
-	DELETE FROM `sysaux`.`log` WHERE DATEDIFF(DATE_FORMAT(NOW(), '%Y-%m-%d'), DATE_FORMAT(`log_time`, '%Y-%m-%d')) > 30;
+	DELETE FROM `mysysutl`.`log` WHERE DATEDIFF(DATE_FORMAT(NOW(), '%Y-%m-%d'), DATE_FORMAT(`log_time`, '%Y-%m-%d')) > 30;
 END$$
 
 DELIMITER ;
